f1b13f155ed9d1274f7ef69aa28e5d15b5fa86ff
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-15 22:05:00 -0400

    defined reviews container so that when reviews-container is called on it will know what it is

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// let titleInput = () => document.getElementById("title")
+// let authorInput = () => document.getElementById("author")
+// let contentInput = () => document.getElementById("content")
+let reviewsContainer = document.getElementById("reviews-container")
+
 const startProgram = () => {
 
 }

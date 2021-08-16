519d516df03a0a7a2a4b7fe745f3be393196c0f2
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-16 03:31:18 -0400

    created a create method in the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,14 @@
 class CommentsController < ApplicationController
 
+    def create 
+        comment = Comment.new(comment_params)
+        if comment.save 
+            render json: Review.all.to_json(:include => :comments)
+        else
+
+        end
+    end
+
     def update 
         comment = Comment.find(params[:id])
         if comment.update(comment_params)

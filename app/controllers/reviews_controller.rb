55db62e4c014eb7e7bbc0143dc50a7e83ade8d8b
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-16 00:43:52 -0400

    created an update method and a review params method in the reviews controller that will allow a review to be update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,19 @@ class ReviewsController < ApplicationController
         render json: reviews.to_json(:include => :comments)
     end
 
+    def update 
+        review = Review.find(params)
+        if review.update(review_params)
+            render json: Review.to_json(:include => :comments)
+        else
+            render json: {errors: "There was an error"}
+        end
+    end
+
+    private 
+
+    def review_params
+        params.require(:review).permit(:title, :author, :content, :likes)
+    end
+
 end

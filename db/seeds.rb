48d3d34d82b073aa9ef26a4cb30a06a65994e976
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-15 21:23:17 -0400

    added basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Review.create(title:"")
+Review.create(title:"The Perks of Being a Wallflower", author:"Stephen Chbosky", content:"Socially awkward teen Charlie (Logan Lerman) is a wallflower, always watching life from the sidelines, until two charismatic students become his mentors. Free-spirited Sam (Emma Watson) and her stepbrother Patrick (Ezra Miller) help Charlie discover the joys of friendship, first love, music and more, while a teacher sparks Charlie's dreams of becoming a writer. However, as his new friends prepare to leave for college, Charlie's inner sadness threatens to shatter his newfound confidence.")
+Review.create(title:"The Fault In Our Stars", author:"John Green", content:"The Fault In Our Stars is a fabulous book about a young teenage girl who has been diagnosed with lung cancer and attends a cancer support group. Hazel is 16 and is reluctant to go to the support group, but she soon realises that it was a good idea. Hazel meets a young boy named Augustus Waters.")
+Review.create(title:"The Great Gatsby", author:"F. Scott Fitzgerald", content:"The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan")
+
+Comment.create(content:"I love this book so much!", post_id: 1)
+Comment.create(content:"Wish I could read this for the first time again!", post_id: 1)
+
+Comment.create(content:"Great book!", post_id: 2)
+Comment.create(content:"Kinda over-rated but I like it!", post_id: 2)

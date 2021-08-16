2210b2b989861893cc78e739bb49f19f566094ad
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-15 21:48:36 -0400

    accidentially put the constructor for the review class in the comment class, snow in the proper place

--- models/Comment.js
+++ models/Comment.js
@@ -1,10 +1,5 @@
 class Comment {
-    constructor(id, title, author, content, likes, comments){
-        this.id = id
-        this.title = title
-        this.author = author
-        this.content = content 
-        this.likes = likes 
-        this.comments = [...comments] 
+    constructor(){
+        
     }
 }
--- models/Review.js
+++ models/Review.js
@@ -1,3 +1,10 @@
 class Review {
-    
+    constructor(id, title, author, content, likes, comments){
+        this.id = id
+        this.title = title
+        this.author = author
+        this.content = content 
+        this.likes = likes 
+        this.comments = [...comments] 
+    }
 }

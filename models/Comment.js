32de40583e513e622874d9d05e7352972e509a64
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-16 02:23:57 -0400

    created a function called renderComments where I defined elements and set them to the corresponding elements

--- models/Comment.js
+++ models/Comment.js
@@ -5,4 +5,23 @@ class Comment {
         this.likes = likes 
         this.review_id = review_id 
     }
+
+    static renderComments(comments){
+        let reviewComments = comments.map(comment => {
+            let li = document.createElement("li")
+            let div = document.createElement("div")
+            let commentContent = document.createElement("p")
+            let commentLikes = document.createElement("p")
+            let likeButton = document.createElement("button")
+            commentContent.innerText = comment.content
+            commentLikes.innerText = comment.likes 
+            likeButton.innerText = "♥"
+            div.appendChild(commentContent)
+            div.appendChild(commentLikes)
+            div.appendChild(likeButton)
+            li.appendChild(div)
+            return li
+        })
+        return reviewComments
+    }
 }
